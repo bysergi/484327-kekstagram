d40c596b11af19f8da4d93eeacd148956831283c
Sergi <rockncream@gmail.com>
2017-08-31 11:28:02 +0300

    Исправил функцию, данные загружаются на страницу

--- js/pictures.js
+++ js/pictures.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var pictures = [];
-var comments = [
+var COMMENTS = [
   "Всё отлично!",
   "В целом всё неплохо. Но не всё.",
   "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.",
@@ -9,50 +9,62 @@ var comments = [
   "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.",
   "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!"
 ];
-var randomComments = [];                                           // Случайны комменты к фотографии
-var picturePage = document.querySelector('.gallery-overlay');      // Находим оверлей
-var pictureTemplate = document.querySelector('#picture-template'); // Находим темплейт
 
+var picturePage = document.querySelector('.gallery-overlay'); 
+var pictureTemplate = document.querySelector('#picture-template');
+
+var getTrueOrFalse = function () {
+  var TrueOrFalse = Math.random() >= 0.5;
+  return TrueOrFalse;
+}
 var addComments = function () { // Создание массива комментов к фотографии
-  var randomBoolean = Math.random() >= 0.5
+  var randomBoolean = getTrueOrFalse();
+  var randomComments = [];
   if (randomBoolean == true) {  // Если тру, то добавляем два коммента
     for (var i = 0; i < 2; i++) {
       var index = Math.floor(Math.random() * (5 - i)) + i;
-      randomComments.push(comments[index]);
+      if (i === 1) {
+        COMMENTS[index] = ' ' + COMMENTS[index];
+      }
+      randomComments.push(COMMENTS[index]);
     }
     return randomComments;
   }
   else {                        // Если фолс, то добавляем один коммент
     var index = Math.floor(Math.random() * 5);
-    randomComments.push(comments[index]);
+    randomComments.push(COMMENTS[index]);
     return randomComments;
   }
 }
-var createPicData = function (i) { // Функция для создания данных к фотографии
-  var picData = {
-    url: 'photos/' + i + '.jpg',
-    likes: Math.floor((Math.random() * 185) + 15),
-    comments: addComments()        // Массив из 1 или 2 строк
+
+var getPictureData = function () { // Функция для создания данных к фотографиям
+  for (i = 1; i < 27; i++) {
+    var pictureData = {
+      url: 'photos/' + i + '.jpg',
+      likes: Math.floor((Math.random() * 185) + 15),
+      comments: addComments()        // Массив из 1 или 2 строк
+    }
+    pictures.push(pictureData);
   }
 }
-var fillPicPage = function (picture) { // Функция заполенния данными
-  picturePage.querySelector('.gallary-overlay-image').src = pictures.url;
-  picturePage.querySelector('.likes-count').textContent = pictures.likes;
-  picturePage.querySelector('.comments-count').textContent = pictures.comments;
-}
+getPictureData();
 
-var fragment = document.createDocumentFragment();                  // Создаем фрагмент, в который будем класть элемент
-for (var i = 0; i < 25; i++) {
+var fragment = document.createDocumentFragment(); // Создаем фрагмент, в который будем класть элементы
+for (var i = 0; i < pictures.length; i++) {
   var picture = pictureTemplate.content.cloneNode(true);
-  createPicData(i);
-  // picture.querySelector('img').src = picData.url;                  // picData не отдает данные
-  // picture.querySelector('.picture-likes').textContent = picData.likes;
-  // picture.querySelector('.picture-comments').textContent = picData.comments;
-  // pictures.push(picture);                                          // Добавляем данные в массив пикчерс
+  picture.querySelector('img').src = pictures[i].url;
+  picture.querySelector('.picture-likes').textContent = pictures[i].likes;
+  picture.querySelector('.picture-comments').textContent = pictures[i].comments;
   fragment.appendChild(picture);
 }
 
-document.querySelector('.pictures').appendChild(fragment); // отрисовали в блок пикчерс
+var fillPicturePage = function (pictureIndex) { // Функция заполенния данными страницы фотографии
+  picturePage.querySelector('.gallery-overlay-image').src = pictureIndex.url;
+  picturePage.querySelector('.likes-count').textContent = pictureIndex.likes;
+  picturePage.querySelector('.comments-count').textContent = pictureIndex.comments;
+}
+
+document.querySelector('.pictures').appendChild(fragment);
 document.querySelector('.upload-ovelay').classList.add('hidden');
-picturePage.classList.remove('hidden');                    // Показываем оверлей
-fillPicPage(picture[0]);                                   // Заполняем данные из первой фотографии
+picturePage.classList.remove('hidden');      
+fillPicturePage(pictures[0]);
