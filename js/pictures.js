615facf5719b022a936c683d434adbd23a4d9e9f
Sergi <rockncream@gmail.com>
2017-09-05 00:37:53 +0300

    #11 Подробности: добавил обработчики на октрытие-закыртые фотографий

--- js/pictures.js
+++ js/pictures.js
@@ -8,9 +8,13 @@ var COMMENTS = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
+var PHOTOS_SUM = 26;
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
 var picturePage = document.querySelector('.gallery-overlay');
 var pictureTemplate = document.querySelector('#picture-template');
+var picturePageClose = document.querySelector('.gallery-overlay-close');
 
 var getRandomBoolean = function () {
   return Math.random() >= 0.5;
@@ -41,29 +45,88 @@ var getPicturesData = function (num) {
   }
   return pictures;
 };
+
+// Наполяняем страницу фотками
 var createPicturesGrid = function (pictures) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < pictures.length; i++) {
-    var picture = pictureTemplate.content.cloneNode(true);
-    picture.querySelector('img').src = pictures[i].url;
-    picture.querySelector('.picture-likes').textContent = pictures[i].likes;
-    picture.querySelector('.picture-comments').textContent = pictures[i].comments;
-    fragment.appendChild(picture);
+    var template = pictureTemplate.content.cloneNode(true);
+    template.querySelector('img').src = pictures[i].url;
+    template.querySelector('.picture-likes').textContent = pictures[i].likes;
+    template.querySelector('.picture-comments').textContent = pictures[i].comments;
+    fragment.appendChild(template);
   }
   return fragment;
 };
-var fillPicturePage = function (picture) { // Функция заполенния данными страницы фотографии
-  var fragment = document.createDocumentFragment();
-  picturePage.querySelector('.gallery-overlay-image').src = picture.url;
-  picturePage.querySelector('.likes-count').textContent = picture.likes;
-  picturePage.querySelector('.comments-count').textContent = picture.comments.length;
-  fragment.appendChild(picture);
-  return fragment;
+
+// Заполняем страницу фото данными из первью
+var fillPicturePage = function (picturePreview) {
+  picturePage.querySelector('.gallery-overlay-image').src = picturePreview.url;
+  picturePage.querySelector('.likes-count').textContent = picturePreview.likes;
+  picturePage.querySelector('.comments-count').textContent = picturePreview.comments.length;
 };
 
-var pictures = getPicturesData(26);
+// Берем данные на из превью
+var getPicturePreviewData = function (event) {
+  var PicturePreviewData = {
+    url: event.currentTarget.children[0].getAttribute('src'),
+    likes: event.currentTarget.querySelector('.picture-likes').textContent,
+    comments: event.currentTarget.querySelector('.picture-comments').textContent
+  };
+  return PicturePreviewData;
+};
+
+// Добавление и удаление обработчиков
+var openPicPage = function () {
+  picturePage.classList.remove('hidden');
+  picturePageClose.setAttribute('tabindex', 0);
+  picturePageClose.addEventListener('click', onPicPageClose);
+  document.addEventListener('keydown', onPicPageEscPress);
+  picturePageClose.addEventListener('keydown', onPicPageEnterPress);
+};
+var closePicPage = function () {
+  picturePage.classList.add('hidden');
+  picturePageClose.removeEventListener('click', onPicPageClose);
+  document.removeEventListener('keydown', onPicPageEscPress);
+  picturePageClose.removeEventListener('keydown', onPicPageEnterPress);
+};
+
+// Обработчики
+var onPicPageClose = function () {
+  picturePage.classList.add('hidden');
+};
+var onPicPageEscPress = function (event) {
+  if (event.keyCode === ESC_KEYCODE) {
+    closePicPage();
+  }
+};
+var onPicPageEnterPress = function (event) {
+  if (event.keyCode === ENTER_KEYCODE) {
+    closePicPage();
+  }
+};
+var onPicPreviewClick = function (event) {
+  event.preventDefault();
+  var picturePageData = getPicturePreviewData(event);
+  fillPicturePage(picturePageData);
+  openPicPage();
+};
+var onPicPreviewEnterPress = function (event) {
+  if (event.keyCode === ENTER_KEYCODE) {
+    openPicPage();
+  }
+};
+
+var pictures = getPicturesData(PHOTOS_SUM);
 var picturesGrid = createPicturesGrid(pictures);
 document.querySelector('.pictures').appendChild(picturesGrid);
-// document.querySelector('.upload-ovelay').classList.add('hidden');
-picturePage.classList.remove('hidden');
-fillPicturePage(pictures[0]);
+document.querySelector('.upload-overlay').classList.add('hidden');
+// picturePage.classList.remove('hidden');
+// fillPicturePage(pictures[0]);
+var createdPicPreviews = document.querySelectorAll('.picture');
+for (var i = 0; i < createdPicPreviews.length; i++) {
+  createdPicPreviews[i].setAttribute('tabindex', 0);
+  createdPicPreviews[i].addEventListener('click', onPicPreviewClick);
+  createdPicPreviews[i].addEventListener('keydown', onPicPreviewEnterPress);
+}
+
