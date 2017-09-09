d408b5d3e45120b1334b5a9e472c4359acc6e6a8
Sergi <rockncream@gmail.com>
2017-09-09 16:23:44 +0300

    #12 Доверяй, но проверяй: скрытие/показ формы,управление фотографией, валидация

--- js/pictures.js
+++ js/pictures.js
@@ -11,10 +11,30 @@ var COMMENTS = [
 var PHOTOS_SUM = 26;
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
+var STEP = 25;
+var MIN_SIZE = 25;
+var MAX_SIZE = 100;
+var MAX_TAGS = 5;
+var MAX_TAG_LENGTH = 21;
+var MAX_COMMENTS_LENGTH = 100;
 
 var picturePage = document.querySelector('.gallery-overlay');
 var pictureTemplate = document.querySelector('#picture-template');
 var picturePageClose = document.querySelector('.gallery-overlay-close');
+var cropFormPage = document.querySelector('.upload-overlay');
+var uploadElem = document.querySelector('#upload-select-image');
+var uploadFile = uploadElem.querySelector('#upload-file');
+var uploadForm = uploadElem.querySelector('.upload-image');
+var cropFormCancel = document.querySelector('.upload-form-cancel');
+var buttonSubmit = document.querySelector('#upload-submit');
+var formComments = document.querySelector('.upload-form-description');
+var formTags = document.querySelector('.upload-form-hashtags');
+var resizeValue = document.querySelector('.upload-resize-controls-value');
+var decImageSize = document.querySelector('.upload-resize-controls-button-dec');
+var incImageSize = document.querySelector('.upload-resize-controls-button-inc');
+var imageSize = document.querySelector('.effect-image-preview');
+var effectControls = document.querySelector('.upload-effect-controls');
+var uploadSelectImageForm = document.querySelector('#upload-select-image');
 
 var getRandomBoolean = function () {
   return Math.random() >= 0.5;
@@ -130,3 +150,129 @@ for (var i = 0; i < createdPicPreviews.length; i++) {
   createdPicPreviews[i].addEventListener('keydown', onPicPreviewEnterPress);
 }
 
+var openCropFrom = function () {
+  cropFormPage.classList.remove('hidden');
+  cropFormCancel.addEventListener('click', onCropFormCancel);
+  document.addEventListener('keydown', onCropFormEscPress);
+  cropFormCancel.addEventListener('keydown', onCloseCropFormEnterPress);
+};
+var closeCropForm = function () {
+  cropFormPage.classList.add('hidden');
+  uploadForm.classList.remove('hidden');
+  cropFormCancel.removeEventListener('click', onCropFormCancel);
+  document.removeEventListener('keydown', onCropFormEscPress);
+  cropFormCancel.removeEventListener('keydown', onCloseCropFormEnterPress);
+};
+
+var onUploadFile = function () {
+  uploadForm.classList.add('hidden');
+  openCropFrom();
+};
+var onCropFormCancel = function () {
+  closeCropForm();
+};
+var onCropFormEscPress = function (event) {
+  if (event.keyCode === ESC_KEYCODE) {
+    closeCropForm();
+  }
+};
+var onCloseCropFormEnterPress = function (event) {
+  if (event.keyCode === ENTER_KEYCODE) {
+    closeCropForm();
+  }
+};
+
+var setFormSubmit = function () {
+  uploadSelectImageForm.setAttribute('action', 'https://1510.dump.academy/kekstagram');
+  uploadSelectImageForm.setAttribute('method', 'post');
+  uploadSelectImageForm.setAttribute('enctype', 'multipart/form-data');
+};
+var setFormComments = function () {
+  formComments.setAttribute('maxlength', MAX_COMMENTS_LENGTH);
+};
+var changeImageSize = function (direction) {
+  var newSize = parseInt(resizeValue.value, 10) + STEP * direction;
+  if (newSize >= MIN_SIZE && newSize <= MAX_SIZE) {
+    resizeValue.value = newSize + '%';
+    imageSize.style.transform = 'scale(' + newSize / 100 + ')';
+  }
+};
+
+var changeImageEffect = function (effect) {
+  imageSize.classList.remove(currentEffect);
+  currentEffect = 'effect-' + effect.value;
+  imageSize.classList.add(currentEffect);
+};
+var onEffectControlsClick = function () {
+  var target = event.target;
+  if (target.tagName.toLowerCase() === 'input') {
+    changeImageEffect(target);
+  }
+};
+
+var checkComments = function () {
+  turnFormToNormal(formComments);
+};
+var checkTags = function () {
+  turnFormToNormal(formTags);
+  formTags.setCustomValidity('');
+  if (formTags.value.length === 0) {
+    return;
+  }
+  checkNumberSign();
+};
+var checkNumberSign = function () {
+  var tagsList = formTags.value.match(/\#[a-zA-Zа-яА-Я0-9\-]+/g);
+  if (tagsList === null) {
+    formTags.setCustomValidity('Первый символ должен быть «#»');
+  } else {
+    if (tagsList.length > MAX_TAGS) {
+      formTags.setCustomValidity('Максимум 5 тегов');
+    }
+    for (i = 0; i < tagsList.length; i++) {
+      if (tagsList[i].length > MAX_TAG_LENGTH) {
+        formTags.setCustomValidity('Максимум 20 символов в одном теге');
+        return;
+      }
+    }
+  }
+};
+
+var turnFormToError = function (elem) {
+  elem.style.border = '4px solid red';
+};
+var turnFormToNormal = function (elem) {
+  elem.style.border = elem.styleBorderOrigin;
+};
+
+var onSubmitForm = function (event) {
+  if (formComments.checkValidity() === false) {
+    turnFormToError(formComments);
+  }
+  if (formTags.checkValidity() === false) {
+    turnFormToError(formTags);
+  }
+};
+
+
+uploadFile.addEventListener('change', onUploadFile);
+cropFormCancel.style.left = '90%'; // подвинул, чтобы кнопка влезала в экран
+setFormComments();
+effectControls.addEventListener('click', onEffectControlsClick);
+
+var currentEffect = null;
+resizeValue.value = 100 + '%';
+decImageSize.addEventListener('click', function () {
+  changeImageSize(-1);
+});
+incImageSize.addEventListener('click', function () {
+  changeImageSize(1);
+});
+
+formTags.addEventListener('input', checkTags);
+formComments.addEventListener('input', checkComments);
+setFormSubmit();
+
+formComments.styleBorderOrigin = formComments.style.border;
+formTags.styleBorderOrigin = formTags.style.border;
+buttonSubmit.addEventListener('click', onSubmitForm);
